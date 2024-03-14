d96c3f236acab602784edbedeec10796f8d9d729
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2024-03-14 14:12:28 +0400

    fix symlinks

--- symlinks.sh
+++ symlinks.sh
@@ -1,11 +1,10 @@
 # iTerm2
 rm "${HOME}/Library/Preferences/com.googlecode.iterm2.plist"
-ln -s -f ./iterm2/com.googlecode.iterm2.plist "${HOME}/Library/Preferences/com.googlecode.iterm2.plist"
+ln -s -f $(pwd)/iterm2/com.googlecode.iterm2.plist "${HOME}/Library/Preferences/com.googlecode.iterm2.plist"
 
 # Git
-ln -s -f ./git/.gitconfig ~/.gitconfig
-ln -s -f ./git/.gitignore_global ~/.gitignore_global
+ln -s -f $(pwd)/git/.gitconfig ~/.gitconfig
+ln -s -f $(pwd)/git/.gitignore_global ~/.gitignore_global
 
 # Fish
-ln -s -f ./fish ~/.config/fish
-ln -s -f ./omf ~/.config/omf
+ln -s $(pwd)/fish ~/.config/fish
